864a34de1c361cc7deadd09236d296aa1092f897
Tim White <walton.timothy.james.white@gmail.com>
2023-12-08 15:25:52 +1300

    Rename Perl script

--- convert-json-to-tsv-faster.sh
+++ convert-json-to-tsv-faster.sh
@@ -2,4 +2,4 @@
 
 /bin/echo -e "container_1\tcontainer_2\tclass_1\tclass_2\tdescription\tline\tmethod_descriptor\tmethod_name\tmutator"
 
-./convert-json-to-tsv-faster.pl | jq -r '[.inJar, .outJar, .inClass, .outClass, .d.description, .d.location.line, .d.location."method-descriptor", .d.location."method-name", .d.mutator] | join("\t")' | sort
+./make-json-stream-with-filename-metadata.pl | jq -r '[.inJar, .outJar, .inClass, .outClass, .d.description, .d.location.line, .d.location."method-descriptor", .d.location."method-name", .d.mutator] | join("\t")' | sort
