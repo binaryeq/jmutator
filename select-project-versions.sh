48bedf30f831c443cfa270dae080a950fe87c6b2
Tim White <walton.timothy.james.white@gmail.com>
2023-12-19 16:03:21 +1300

    Improve comments

--- select-project-versions.sh
+++ select-project-versions.sh
@@ -12,7 +12,7 @@ then
 	exit 1
 fi
 
-#for f in $JARS/*/*.jar; do echo ${f##*/}; done|sort|uniq > project_names.txt
+# Read project and jar names from dataset.json. Optimistically add test jars; later processing will need to filter out any that don't exist.
 jq -r '.[] | .name + "\t" + .jar' < "$DATASET" > project_names.txt
 perl -le 'sub svcmp($$) { my ($x, $y) = @_; return 0 if !@$x && !@$y; return -1 if !@$x; return 1 if !@$y; return ($x->[0] <=> $y->[0]) || svcmp([@$x[1..$#{$x}]], [@$y[1..$#{$y}]]); } sub getsv($) { my ($before, $sv, $after) = $_[0] =~ m|^(\S+)\t.+-([\d.]+)((?:-tests)?\.jar)$| or die "Cannot find semver in $_[0]"; return ([ split /\./, $sv ], "$after$before"); } my @lines = <>; chomp @lines; @lines = sort { my ($asv, $arest) = getsv($a); my ($bsv, $brest) = getsv($b); return ($arest cmp $brest) || svcmp($asv, $bsv); } @lines; for (my $i = 0; $i < @lines; ++$i) { print $lines[$i] if ($i == @lines - 1 || (getsv($lines[$i]))[1] ne (getsv($lines[$i + 1]))[1]); }' < project_names.txt > latest_project_versions.txt
 perl -lane 'print $F[1]; $F[1] =~ s|\.jar$|-tests.jar|; print $F[1]' < latest_project_versions.txt > latest_project_versions_with_tests.txt
